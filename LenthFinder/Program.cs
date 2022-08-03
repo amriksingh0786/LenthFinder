5901a78b4360d63c451860192fc1a05019621aa5
Amrik Singh Khalsa <amriksingh.khalsa@honeywell.com>
2022-08-03 18:07:28 +0530

    Added unit test cases and added class

--- LenthFinder/Program.cs
+++ LenthFinder/Program.cs
@@ -1,24 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-int selectionFromUser = 0;
-Console.WriteLine("Welcome to the C#\n Lets go through the Menu\n 1. Press 1 for finding length of String \n Press 2 for Finding number of Characters \n Press 3 to exit");
-selectionFromUser = Int32.Parse( Console.ReadLine());
-switch (selectionFromUser)
+﻿namespace LengthHelperNamespace
 {
-    case 1:
-        string stringInputFromUser = "";
-        Console.WriteLine("Enter String to get Length\n");
-        stringInputFromUser = Console.ReadLine();
-        int lengthOfString = stringInputFromUser.Length;
-        Console.WriteLine("The string Entered is " + stringInputFromUser);
-        Console.WriteLine("The Length of String is " + lengthOfString);
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    default:
-        Console.WriteLine("Please Choose Correct Option");
-            break;
-
-
+    public class LengthHelper
+    {
+        public static void Main()
+        {
+            LengthHelper lengthHelper = new LengthHelper();
+            Console.WriteLine("Please enter somehing to get length of it: ");
+            string input = Console.ReadLine();
+            int length = lengthHelper.GetLength(input);
+            Console.WriteLine(length);
+        }
+        public int GetLength(string text)
+        {
+            return text.Length;
+        }
+    }
 }
